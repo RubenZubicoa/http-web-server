8872df74581b6731160b5830dddb7d1617b927b9
Ruben Zubicoa <rubenzubicoatic@gmail.com>
2021-06-15 19:54:37 +0200

    feat(server): create the server

---
+++ server.js
@@ -0,0 +1,20 @@
+'use strict'
+const http = require('http')
+const PORT = process.env.PORT || 3000
+
+const hello = `
+<html>
+    <head></head>
+    <body>
+        <h1>Hello world</h1>
+    </body>
+</html>
+`
+
+const server = http.createServer((req, res) => {
+    res.setHeader('content-Type', 'text/html')
+    res.end(hello)
+})
+
+server.listen(PORT)
+
